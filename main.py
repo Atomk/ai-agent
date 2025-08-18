d79a2b4e3ed6b5d701153d2d9093e90be97c0df2
Atomk <marco.fras@hotmail.it>
2025-08-19 00:18:42 +0200

    feat: verbose flag
    Allow to print extra information about the LLM response.

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
+    verbose = False
+    if sys.argv[-1] == "--verbose":
+        verbose = True
+        sys.argv.pop()
+
     if len(sys.argv) != 2:
         print("ERROR: you need to provide the prompt as an argument")
         print("Usage: uv run main.py \"Is this a prompt?\"")
@@ -22,11 +27,15 @@ def main():
     response = client.models.generate_content(model=MODEL, contents=prompt)
 
     print(response.text)
-    if response.usage_metadata:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    else:
-        print("ERROR: API usage data not available")
+
+    if verbose:
+        print(f"User prompt: {prompt}")
+        if response.usage_metadata:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+            print(f"Total tokens: {response.usage_metadata.total_token_count}")
+        else:
+            print("ERROR: API usage data not available")
 
 if __name__ == "__main__":
     main()
