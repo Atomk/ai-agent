1b788c3d3d0dd6d8a85bd583ecbdf6491672b98b
Atomk <marco.fras@hotmail.it>
2025-08-23 21:37:27 +0200

    style: use docstrings for config constants

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 MODEL_ID = "gemini-2.0-flash-001"
 
-# Stop subcommand execution if program runs longer than the allowed time
 SUBCOMMAND_TIMEOUT_SECONDS = 30
+"""Maximum allowed execution time for a function called by the agent.
+Subcommand execution should be terminated if it runs longer than this amount of seconds."""
 
-# Limits the number of characters that the agent can read from a file.
-# Prevents accidentally sending huge files to the LLM.
 MAX_FILE_CONTENT_LENGTH = 10_000
+"""Limits the number of characters that the agent can read from a file.
+Prevents accidentally sending huge files to the LLM."""
 
 WORKING_DIRECTORY = "./calculator"
 """The functions executed by the agent should be able to access
