9320d9f7100ad785f71f933a5364cdb0f7f0b1b8
Atomk <marco.fras@hotmail.it>
2025-09-01 23:45:42 +0200

    refactor> extract function call stuff to separate file
    I saw the the official Boot.dev solution does this and to some degree it makes sense to me, decluttering the main fila is nice. Also I guess there-s some locality of behavior since the purpose of the new module is to gather what-s needed to communicate the LLM what functions it can call.

---
+++ call_function.py
@@ -0,0 +1,90 @@
+from google.genai import types
+
+from config import WORKING_DIRECTORY
+from functions.get_files_info import get_files_info, schema_get_file_info
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.run_python import run_python_file, schema_run_python_file
+from functions.write_file import write_file, schema_write_file
+
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_file_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file,
+    ]
+)
+
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
+
+If you are not sure what file or project the user is talking about, you can look at the contents of the root directory to see if that provides enough context to perform the requested action.
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+
+# TODO add tests
+def call_function(
+    function_call_part: types.FunctionCall,
+    verbose=False,
+) -> types.Content:
+    if verbose:
+        print(f" - Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+
+    function_name = function_call_part.name
+    if function_name is None:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name="call_function",
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+
+    function_result: str
+    match function_name:
+        case "get_files_info": function_result = get_files_info(WORKING_DIRECTORY, **function_call_part.args) # type: ignore
+        case "get_file_content": function_result = get_file_content(WORKING_DIRECTORY, **function_call_part.args) # type: ignore
+        case "run_python_file": function_result = run_python_file(WORKING_DIRECTORY, **function_call_part.args) # type: ignore
+        case "write_file": function_result = write_file(WORKING_DIRECTORY, **function_call_part.args) # type: ignore
+        case _:
+            #return f"Error: function call for \"{function_call_part.name}\" not implemented."
+            return types.Content(
+                role="tool",
+                parts=[
+                    types.Part.from_function_response(
+                        name=function_name,
+                        response={"error": f"Unknown function: {function_name}"},
+                    )
+                ],
+            )
+
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
+
+
+__all__ = [
+    "available_functions",
+    "system_prompt",
+    "call_function",
+]
--- main.py
+++ main.py
@@ -4,60 +4,12 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-from config import MODEL_ID, WORKING_DIRECTORY, MAX_ITERATIONS
-from functions.get_files_info import get_files_info, schema_get_file_info
-from functions.get_file_content import get_file_content, schema_get_file_content
-from functions.run_python import run_python_file, schema_run_python_file
-from functions.write_file import write_file, schema_write_file
-
-
-def call_function(
-    function_call_part: types.FunctionCall,
-    verbose=False,
-) -> types.Content:
-    if verbose:
-        print(f" - Calling function: {function_call_part.name}({function_call_part.args})")
-    else:
-        print(f" - Calling function: {function_call_part.name}")
-
-    function_name = function_call_part.name
-    if function_name is None:
-        return types.Content(
-            role="tool",
-            parts=[
-                types.Part.from_function_response(
-                    name="call_function",
-                    response={"error": f"Unknown function: {function_name}"},
-                )
-            ],
-        )
-
-    function_result: str
-    match function_name:
-        case "get_files_info": function_result = get_files_info(WORKING_DIRECTORY, **function_call_part.args) # type: ignore
-        case "get_file_content": function_result = get_file_content(WORKING_DIRECTORY, **function_call_part.args) # type: ignore
-        case "run_python_file": function_result = run_python_file(WORKING_DIRECTORY, **function_call_part.args) # type: ignore
-        case "write_file": function_result = write_file(WORKING_DIRECTORY, **function_call_part.args) # type: ignore
-        case _:
-            return types.Content(
-                role="tool",
-                parts=[
-                    types.Part.from_function_response(
-                        name=function_name,
-                        response={"error": f"Unknown function: {function_name}"},
-                    )
-                ],
-            )
-
-    return types.Content(
-        role="tool",
-        parts=[
-            types.Part.from_function_response(
-                name=function_name,
-                response={"result": function_result},
-            )
-        ],
-    )
+from config import MODEL_ID, MAX_ITERATIONS
+from call_function import (
+    call_function,
+    available_functions,
+    system_prompt,
+)
 
 
 def main():
@@ -84,28 +36,6 @@ def main():
 
     # Gemini Client
     client = genai.Client(api_key=api_key)
-
-    available_functions = types.Tool(
-        function_declarations=[
-            schema_get_file_info,
-            schema_get_file_content,
-            schema_run_python_file,
-            schema_write_file,
-        ]
-    )
-
-    system_prompt = """
-    You are a helpful AI coding agent.
-
-    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
-
-    - List files and directories
-    - Read file contents
-    - Execute Python files with optional arguments
-    - Write or overwrite files
-
-    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-    """
 
     if verbose:
         print(f"User prompt: {prompt}")
