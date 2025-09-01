941ce83298a6225cf5d680ea8f48f260538ba23a
Atomk <marco.fras@hotmail.it>
2025-09-02 01:11:40 +0200

    fix: missing config variable

--- config.py
+++ config.py
@@ -11,3 +11,7 @@ Prevents accidentally sending huge files to the LLM."""
 WORKING_DIRECTORY = "./calculator"
 """The functions executed by the agent should be able to access
 only files and subdirectories located within this directory."""
+
+MAX_ITERATIONS = 15
+"""Maximum number of times the agent can iterate over the results of its actions.
+This helps preventing infinite loops and wasting tokens."""
